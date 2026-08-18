3bc21aed1429a1ba2bed773581c5717ed77057f6
alh1-pixel <alhafizrpl1@gmail.com>
2026-08-18 13:44:53 +0700

    LKM-2 tata letak

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Koperasi Sekolah"),),
-        body: const Center(child: Text("Kasir Koperasi"),),
+        body: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Buku Tulis'),
+              const SizedBox(height: 8,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const[
+                  Text('Anggota : Rp. 3.000'),
+                  Text('Umum    : Rp. 3.500'),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
