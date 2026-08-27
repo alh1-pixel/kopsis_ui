2ab2c7c5564ebf0c04a52dd57fc86a00c531cf79
alh1-pixel <alhafizrpl1@gmail.com>
2026-08-27 11:28:06 +0700

    LKM-4 daftar barang

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ IconData getKategoriIcon(String kategori){
       return Icons.edit_note;
     case 'makanan':
       return Icons.fastfood;
-    case 'minukan':
+    case 'minuman':
       return Icons.local_drink;
     default:
       return Icons.inventory_2;
@@ -14,54 +14,41 @@ IconData getKategoriIcon(String kategori){
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  final String kategori1 = 'ATK';
-  final String kategori2 = 'MAKANAN';
-  final int stok = 40;
+  final List<Map<String, dynamic>> daftarBarang = const[
+    {'nama': 'Buku Tulis', 'kategori': 'ATK' ,'anggota' : 3000, 'umum' : 3500, 'stok' : 40},
+    {'nama': 'Pulpen', 'kategori': 'ATK', 'anggota' : 2500, 'umum' : 3000, 'stok' : 25},
+    {'nama': 'Roti', 'kategori': 'MAKANAN', 'anggota' : 5000, 'umum' : 5500, 'stok' : 15},
+    {'nama': 'Susu UHT', 'kategori': 'MINUMAN', 'anggota' : 6000, 'umum' : 6500, 'stok' : 5},
+    {'nama': 'Pensil', 'kategori': 'ATK', 'anggota' : 2000, 'umum' : 2500, 'stok' : 10},
+    {'nama': 'Penghapus', 'kategori': 'ATK', 'anggota' : 2000, 'umum' : 2500, 'stok' : 15},
+    {'nama': 'Donat', 'kategori': 'MAKANAN', 'anggota' : 5000, 'umum' : 5500, 'stok' : 25},
+    {'nama': 'Kopi', 'kategori': 'Minuman', 'anggota' : 7000, 'umum' : 7500, 'stok' : 10},
+    {'nama': 'Kripik', 'kategori': 'MAKANAN', 'anggota' : 5000, 'umum' : 5500, 'stok' : 7},
+    {'nama': 'Kentang Goreng', 'kategori': 'MAKANAN', 'anggota' : 6000, 'umum' : 6500, 'stok' : 4},
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text("Koperasi Sekolah"),),
-        body: ListView(
-          children: [
-            Card(
-              margin: const EdgeInsets.all(12),
-              elevation: 0,
+        body: ListView.builder(
+          itemCount: daftarBarang.length,
+          itemBuilder: (context, index) {
+            final barang = daftarBarang[index];
+            return Card(
+              margin: const EdgeInsets.all(8),
               child: ListTile(
-                leading: Icon(getKategoriIcon(kategori1),
-                size: 32,
-                color: Colors.lightBlue,),
-                title:  const Text(
-                  'Buku Tulis',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                leading:  Icon(
+                  getKategoriIcon(barang['kategori']),
+                  color: Colors.lightBlue,
                 ),
-                subtitle: const Text('Anggota Rp3.000 | Umum Rp3.500'),
-                trailing: Text(
-                  'Stok ' + stok.toString(),
-                  style: TextStyle(color: stok == 0 ? Colors.red : Colors.black),
-                  ),
-              )
-            ),
-            Card(
-              margin: const EdgeInsets.all(12),
-              elevation: 4,
-              child: ListTile(
-                leading: Icon(getKategoriIcon(kategori2),
-                size: 32,
-                color: Colors.lightBlue,),
-                title:  const Text(
-                  'Roti',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                subtitle: const Text('Anggota Rp4.000 | Umum Rp4.500'),
-                trailing: Text(
-                  'Stok ' + stok.toString(),
-                  style: TextStyle(color: stok == 0 ? Colors.red : Colors.black),
-                  ),
-              )
-            ),
-          ],
+                title: Text(barang['nama']),
+                subtitle: Text('Anggota Rp' + barang['anggota'].toString()),
+                trailing: Text('Stok ' + barang['stok'].toString()),
+              ),
+            );
+          }
         ),
       ),
     );
